717d4cded3dc38ac33d1677f928176199bf6f7f2
Dakotah Lambert <49007668+vvulpes0@users.noreply.github.com>
2024-10-24 01:47:47 -0400

    improve factorial_shortmul

--- examples/factorial_shortmul.s
+++ examples/factorial_shortmul.s
@@ -11,15 +11,15 @@ _umul16:
 	push r2
 	push r3
 	ld r2,0
-mloop:	or r1,r1
+	or r1,r1
 	jz mdone
-	ctz r3,r1
+mloop:	ctz r3,r1
 	lsl r0,r3
 	add r2,r0
 	add r3,1
-	lsr r1,r3
 	lsl r0,1
-	j mloop
+	lsr r1,r3
+	jnz mloop
 mdone:	ld r0,r2
 	pop r3
 	pop r2
