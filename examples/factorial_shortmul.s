32e22d27a7a2f3d09b9074e11f623a502d8209fa
Dakotah Lambert <49007668+vvulpes0@users.noreply.github.com>
2024-10-24 00:01:27 -0400

    use ctz for faster mul (short)

--- examples/factorial_shortmul.s
+++ examples/factorial_shortmul.s
@@ -5,17 +5,23 @@ loop:	call _umul16
 	jnz loop
 done:	j done
 
+	;; multiply r0 by r1, store into r0
 _umul16:
 	push r1
 	push r2
+	push r3
 	ld r2,0
 mloop:	or r1,r1
 	jz mdone
-	lsr r1,1
-	addc r2,r0
+	ctz r3,r1
+	lsl r0,r3
+	add r2,r0
+	add r3,1
+	lsr r1,r3
 	lsl r0,1
 	j mloop
 mdone:	ld r0,r2
+	pop r3
 	pop r2
 	pop r1
 	ret
